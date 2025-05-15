d76af2492f94933700b479187c0ce8b93547824e
CatPy123 <catarinalima@sempreceub.com>
2025-05-15 13:58:33 -0300

    Update media.c

--- media.c
+++ media.c
@@ -2,23 +2,23 @@
 
 int main(){
 
-    int nota1, nota2, nota3;
-    int media;
+    float nota1, nota2, nota3;
+    float media;
 
     printf("*** Programa de Cálculo de Média ***\n");
 
     printf("Digite a sua primeira nota: \n");
-    scanf("%d", &nota1);
+    scanf("%f", &nota1);
 
     printf("Digite a sua segunda nota: \n");
-    scanf("%d", &nota2);
+    scanf("%f", &nota2);
 
     printf("Digite a sua terceira nota: \n");
-    scanf("%d", &nota3);
+    scanf("%f", &nota3);
 
-    media = (nota1 + nota2 + nota3) / 3
+    media = (float)(nota1 + nota2 + nota3) / 3
 
-    printf("A média é: %d", media);
+    printf("A média é: %.1f", media);
 
     return 0;
 
